a81a06da427d97135805b3e2a44aec4f9a59ee51
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2022-02-12 21:58:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ def main():
   st.sidebar.header("Uber fare Cost prediction")
   st.sidebar.text("This a Web app that tells you the predicted fare costs for Uber.")
   st.sidebar.header("Just fill in the information below")
-  st.sidebar.text("The XGboost Regressor Model was used.")
+  st.sidebar.text("The GradientBoostingRegressor Model was used.")
 
 
 
   pickup_longitude = st.slider("Input Your Pickup Longitude", -180, 180)
   pickup_latitude = st.slider("Input your Pickup Latitude",-90,90)
   dropoff_longitude= st.slider("Input your Dropoff Longitude", -180, 180)
-  dropoff_latitude= st.slider("Input your Dropoff Ltitude",-90,90)
-  passenger_count= st.slider("Input your passenger Count", 1,6)
+  dropoff_latitude= st.slider("Input your Dropoff Latitude",-90,90)
+  passenger_count= st.slider("Input your passenger Count", 1,8)
 
   inputs = [[pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count]]
 
