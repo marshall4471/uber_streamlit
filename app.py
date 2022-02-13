35ef9072a429524258573d17faa15ffcb02b4b78
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2022-02-12 18:57:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,29 @@ import numpy as np
 import string
 import pickle
 st.set_option('deprecation.showfileUploaderEncoding',False) 
-model = pickle.load(open('new_model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 
 def main():
-  st.sidebar.header("Insurance Cost prediction")
-  st.sidebar.text("This a Web app that tells you the predicted insurance costs.")
+  st.sidebar.header("Uber fare Cost prediction")
+  st.sidebar.text("This a Web app that tells you the predicted fare costs for Uber.")
   st.sidebar.header("Just fill in the information below")
-  st.sidebar.text("The AdaBoost regression Model was used.")
+  st.sidebar.text("The XGboost Regressor Model was used.")
 
 
 
-  age = st.slider("Input Your age", 0, 100)
-  sex = st.slider("Input your Gender with 0 for female and 1 for male",0,1)
-  bmi = st.slider("Input your BMI",0.0,70.0)
-  child = st.slider("How many Children",0,10)
-  smoker= st.slider("Are you a smoker 0 for no and 1 for yes",0,1)
+  pickup_longitude = st.slider("Input Your Pickup Longitude", -180, 180)
+  pickup_latitude = st.slider("Input your Pickup Latitude",-90,90)
+  dropoff_longitude= st.slider("Input your Dropoff Longitude", -180 180)
+  dropoff_latitude= st.slider("Input your Dropoff Ltitude",-90,90)
+  passenger_count= st.slider("Input your passenger Count", 1,6)
 
-  inputs = [[age,sex,bmi,child,smoker]]
+  inputs = [[pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count]]
 
   if st.button('Predict'):
     result = model.predict(inputs)
     updated_res = result.flatten().astype(float)
-    st.success('The Your Insurance Costs will be{}'.format(updated_res))
+    st.success('The your predicted fare Costs will be{}'.format(updated_res))
 
 
 if __name__ =='__main__':
